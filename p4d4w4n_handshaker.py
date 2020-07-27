75bac21aa2913fbf820b3f89315cf7d87771bc6d
Hello world <58394579+tzgn@users.noreply.github.com>
2020-07-27 09:36:00 +0300

    Update p4d4w4n_handshaker.py

--- p4d4w4n_handshaker.py
+++ p4d4w4n_handshaker.py
@@ -1,5 +1,5 @@
 import os
-channel=input("router cahnnel: ")
+channel=int(input("router cahnnel: "))
 bssid=input("router mac adress: ")
 writername=input("cap name: ")
 interface=input("interface name: ")
